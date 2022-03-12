c9ca4102e27eafd6d5700fc20a17bcb3baf920c9
reikrad <reikrad@gmail.com>
2022-03-12 17:04:47 -0600

    change foreign

--- database/migrations/2022_03_09_232104_products.php
+++ database/migrations/2022_03_09_232104_products.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('products', function(Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->constrained('users')->nullable(); 
+            $table->foreignId('user_id')->constrained('users'); 
 
             $table->string('title');
             $table->string('image')->nullable();
