97113c9e59ada3220b722ed7ad4441a53d2ae8a8
reikrad <reikrad@gmail.com>
2022-03-12 13:45:19 -0600

    fore

--- database/migrations/2022_03_09_232104_products.php
+++ database/migrations/2022_03_09_232104_products.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function(Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->nullable(); 
+            $table->foreign('user_id')->references('id')->on('users'); 
 
             $table->string('title');
             $table->string('image')->nullable();
