3e2125c4f106dc4f6d45eb8858e35a6ab49caa71
reikrad <reikrad@gmail.com>
2022-03-12 19:51:28 -0600

    bug

--- database/migrations/2022_03_09_232104_products.php
+++ database/migrations/2022_03_09_232104_products.php
@@ -11,9 +11,9 @@ return new class extends Migration
     {
         Schema::create('products', function(Blueprint $table) {
             $table->bigIncrements('id');
-            
+
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->('users');
+            $table->foreign('user_id')->references('id')->on('users');
 
             $table->string('title');
             $table->string('image')->nullable();
