eef5e0515be51607a8273e7b0e37e586576e8d12
reikrad <reikrad@gmail.com>
2022-03-12 22:53:23 -0600

    fix bug

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -31,7 +31,7 @@
             {{ Form::text('inventory', $product->inventory, ['class' => 'form-control' . ($errors->has('inventory') ? ' is-invalid' : ''), 'placeholder' => 'Inventory']) }}
             {!! $errors->first('inventory', '<div class="invalid-feedback">:message</div>') !!}
 
-            <input id="user_id" name="user_id" type="hidden" value="{{Auth::id()}}">
+            <input id="user_id" name="user_id" type="hidden" value='{{Auth::id()}}'>
         </div>
 
     </div>
