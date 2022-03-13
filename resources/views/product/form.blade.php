642339e0dcbf241a51804be69b51b949e9551d6e
reikrad <reikrad@gmail.com>
2022-03-13 00:36:40 -0600

    fix bug

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -30,7 +30,7 @@
             {{ Form::label('Productos en Inventario') }}
             {{ Form::text('inventory', $product->inventory, ['class' => 'form-control' . ($errors->has('inventory') ? ' is-invalid' : ''), 'placeholder' => 'Inventory']) }}
             {!! $errors->first('inventory', '<div class="invalid-feedback">:message</div>') !!}
-            {{ Form::hidden('user_id', $product->user_id ?? Auth::id()) }}
+            {{ Form::hidden('user_id', 1) }}
         </div>
 
     </div>
